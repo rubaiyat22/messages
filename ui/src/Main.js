4eb2dc4181af4689dac2a543a0c8538c44eaa6de
Rubaiyat Rashid <rubaiyat.rashid51@myhunter.cuny.edu>
2019-10-07 22:11:02 -0400

    add ui folder without submodule

---
+++ ui/src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Fade from 'react-reveal/Fade'
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Fade>
+          <h1> HELLO My Wuv </h1>
+        </Fade>
+      </div>
+    )
+  }
+}
